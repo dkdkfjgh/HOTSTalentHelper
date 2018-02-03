9a8f9cf23f718cbdb58169c97b612c0c102afe16
조호연 <dkdkfjgh@naver.com>
2018-02-03 15:33:59 +0900

    First Commit

---
+++ HOTSTalentHelper/MainWindow.xaml.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HOTSTalentHelper
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        HeroAnalyzer HAnalyze = new HeroAnalyzer();
+        int CurrentLevel = 1;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+
+
+
+        }
+        public void SetImagefromUrl(string uri, Image Img)
+        {
+            Byte[] buffer;
+
+            string myUri = uri;
+
+            if (myUri.Substring(0, 4).Equals("http"))
+
+            {
+
+                WebClient wc = new WebClient();
+
+                buffer = wc.DownloadData(new Uri(myUri, UriKind.Absolute));
+
+                wc.Dispose();
+
+            }
+
+            else
+
+            {
+
+                buffer = System.IO.File.ReadAllBytes(myUri);
+
+            }
+
+
+
+            MemoryStream ms = new MemoryStream(buffer);
+
+
+
+            BitmapImage img = new BitmapImage();
+
+            img.BeginInit();
+
+            img.CacheOption = BitmapCacheOption.OnLoad;
+
+            img.StreamSource = ms;
+
+            img.EndInit();
+
+            Img.Source = img;
+        }
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.MouseDown += delegate { DragMove(); };
+        }
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void NextButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (CurrentLevel == 20) return;
+            if (CurrentLevel == 16) { CurrentLevel = 20; UpdateInfo();  return; }
+            CurrentLevel += 3;
+            UpdateInfo();
+        }
+
+
+
+        void UpdateInfo()
+        {
+            LevelText.Content = "Level : " + CurrentLevel.ToString();
+
+            List<Talent> TalentByLevel = new List<Talent>();
+            int level = CurrentLevel;
+            if(HAnalyze.Analyze(textBox.Text) == false)
+            {
+                MessageBox.Show("Failed to Search!");
+            }
+
+            for(int i = 0; i < HAnalyze.SelectedHero.Tal.Count; i++)
+            {
+                if(HAnalyze.SelectedHero.Tal[i].Level == "Level: "+level.ToString())
+                {
+                    TalentByLevel.Add(HAnalyze.SelectedHero.Tal[i].Clone() as Talent);
+                }
+            }
+
+            switch (HAnalyze.TalentCountbyNum(CurrentLevel))
+            {
+                case 1:
+                    SetImagefromUrl(TalentByLevel[0].SkillImageUrl, image1);
+                    TalentName_1.Content = TalentByLevel[0].Name;
+                    TalentDesc_1.Content = TalentByLevel[0].Desc;
+                    TalentPopularity_1.Content = TalentByLevel[0].Popularity;
+                    TalentWinrate_1.Content = TalentByLevel[0].Winrate;
+
+                    image2.Source = null;
+                    TalentName_2.Content = null;
+                    TalentDesc_2.Content = null;
+                    TalentPopularity_2.Content = null;
+                    TalentWinrate_2.Content = null;
+
+                    image3.Source = null;
+                    TalentName_3.Content = null;
+                    TalentDesc_3.Content = null;
+                    TalentPopularity_3.Content = null;
+                    TalentWinrate_3.Content = null;
+
+                    image4.Source = null;
+                    TalentName_4.Content = null;
+                    TalentDesc_4.Content = null;
+                    TalentPopularity_4.Content = null;
+                    TalentWinrate_4.Content = null;
+
+                    break;
+                case 2:
+                    SetImagefromUrl(TalentByLevel[0].SkillImageUrl, image1);
+                    TalentName_1.Content = TalentByLevel[0].Name;
+                    TalentDesc_1.Content = TalentByLevel[0].Desc;
+                    TalentPopularity_1.Content = TalentByLevel[0].Popularity;
+                    TalentWinrate_1.Content = TalentByLevel[0].Winrate;
+
+                    SetImagefromUrl(TalentByLevel[1].SkillImageUrl, image2);
+                    TalentName_2.Content = TalentByLevel[1].Name;
+                    TalentDesc_2.Content = TalentByLevel[1].Desc;
+                    TalentPopularity_2.Content = TalentByLevel[1].Popularity;
+                    TalentWinrate_2.Content = TalentByLevel[1].Winrate;
+
+                    image3.Source = null;
+                    TalentName_3.Content = null;
+                    TalentDesc_3.Content = null;
+                    TalentPopularity_3.Content = null;
+                    TalentWinrate_3.Content = null;
+
+                    image4.Source = null;
+                    TalentName_4.Content = null;
+                    TalentDesc_4.Content = null;
+                    TalentPopularity_4.Content = null;
+                    TalentWinrate_4.Content = null;
+
+                    break;
+                case 3:
+                    SetImagefromUrl(TalentByLevel[0].SkillImageUrl, image1);
+                    TalentName_1.Content = TalentByLevel[0].Name;
+                    TalentDesc_1.Content = TalentByLevel[0].Desc;
+                    TalentPopularity_1.Content = TalentByLevel[0].Popularity;
+                    TalentWinrate_1.Content = TalentByLevel[0].Winrate;
+
+                    SetImagefromUrl(TalentByLevel[1].SkillImageUrl, image2);
+                    TalentName_2.Content = TalentByLevel[1].Name;
+                    TalentDesc_2.Content = TalentByLevel[1].Desc;
+                    TalentPopularity_2.Content = TalentByLevel[1].Popularity;
+                    TalentWinrate_2.Content = TalentByLevel[1].Winrate;
+
+                    SetImagefromUrl(TalentByLevel[2].SkillImageUrl, image3);
+                    TalentName_3.Content = TalentByLevel[2].Name;
+                    TalentDesc_3.Content = TalentByLevel[2].Desc;
+                    TalentPopularity_3.Content = TalentByLevel[2].Popularity;
+                    TalentWinrate_3.Content = TalentByLevel[2].Winrate;
+
+                    image4.Source = null;
+                    TalentName_4.Content = null;
+                    TalentDesc_4.Content = null;
+                    TalentPopularity_4.Content = null;
+                    TalentWinrate_4.Content = null;
+                    break;
+                case 4:
+                    SetImagefromUrl(TalentByLevel[0].SkillImageUrl, image1);
+                    TalentName_1.Content = TalentByLevel[0].Name;
+                    TalentDesc_1.Content = TalentByLevel[0].Desc;
+                    TalentPopularity_1.Content = TalentByLevel[0].Popularity;
+                    TalentWinrate_1.Content = TalentByLevel[0].Winrate;
+
+                    SetImagefromUrl(TalentByLevel[1].SkillImageUrl, image2);
+                    TalentName_2.Content = TalentByLevel[1].Name;
+                    TalentDesc_2.Content = TalentByLevel[1].Desc;
+                    TalentPopularity_2.Content = TalentByLevel[1].Popularity;
+                    TalentWinrate_2.Content = TalentByLevel[1].Winrate;
+
+                    SetImagefromUrl(TalentByLevel[2].SkillImageUrl, image3);
+                    TalentName_3.Content = TalentByLevel[2].Name;
+                    TalentDesc_3.Content = TalentByLevel[2].Desc;
+                    TalentPopularity_3.Content = TalentByLevel[2].Popularity;
+                    TalentWinrate_3.Content = TalentByLevel[2].Winrate;
+
+                    SetImagefromUrl(TalentByLevel[3].SkillImageUrl, image4);
+                    TalentName_4.Content = TalentByLevel[3].Name;
+                    TalentDesc_4.Content = TalentByLevel[3].Desc;
+                    TalentPopularity_4.Content = TalentByLevel[3].Popularity;
+                    TalentWinrate_4.Content = TalentByLevel[3].Winrate;
+                    break;
+            }
+            TalentByLevel.Clear();
+        }
+
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            CurrentLevel = 1;
+            UpdateInfo();
+        }
+
+        private void PrevButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (CurrentLevel == 1) return;
+            if (CurrentLevel == 20) { CurrentLevel = 16; UpdateInfo();  return; }
+            CurrentLevel -= 3;
+            UpdateInfo();
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,311 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Dynamic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web.Script.Serialization;
+using HtmlAgilityPack;
+using agi = HtmlAgilityPack;
+using System.Text.RegularExpressions;
+
+namespace Test
+{
+    class Program
+    {
+        static Hero SelectedHero = new Hero();
+        static void Main(string[] args)
+        {
+            
+            WebClient client = new WebClient();
+            client.Encoding = System.Text.Encoding.UTF8;
+            string getString = client.DownloadString("https://api.hotslogs.com/Public/Data/Heroes");
+
+
+            var serializer = new JavaScriptSerializer();
+            serializer.RegisterConverters(new[] { new DynamicJsonConverter() });
+
+            dynamic obj = serializer.Deserialize(getString, typeof(object));
+
+            List<Hero> HeroesList = new List<Hero>();
+            Hero hero = new Hero();
+            foreach(var items in obj)
+            {
+                hero.PrimaryName = items.PrimaryName;
+                hero.ImageURL = items.ImageURL;
+                hero.AttributeName = items.AttributeName;
+                hero.Group = items.Group;
+                hero.SubGroup = items.SubGroup;
+                hero.Translations = items.Translations;
+                HeroesList.Add(hero.Clone() as Hero);
+            }
+
+            string HeroinKorean;
+            string PrimaryName= "없음";
+
+
+            HeroinKorean = Console.ReadLine(); //입력부!
+
+            
+
+
+            foreach (Hero item in HeroesList)
+            {
+
+                if(item.Translations.Contains(HeroinKorean) == true)
+                {
+                    PrimaryName = item.PrimaryName;
+                    SelectedHero = item.Clone() as Hero;
+                    break;
+                }
+            }
+            
+            if(PrimaryName == "없음")
+            {
+                return;
+            }
+
+            string URL = "https://www.hotslogs.com/Sitewide/HeroDetails?Hero=" + PrimaryName;
+
+            getString = client.DownloadString(URL);
+            agi.HtmlDocument doc = new agi.HtmlDocument();
+            doc.LoadHtml(getString);
+
+            HtmlNode nodes = doc.DocumentNode.SelectSingleNode("//*[@id=\"ctl00_MainContent_RadGridHeroTalentStatistics_ctl00\"]/tbody");
+            Talent talent = new Talent();
+
+            foreach (var node in nodes.ChildNodes)
+            {
+                if (node.ChildNodes.Count == 0) continue;
+                foreach(var item in node.ChildNodes)
+                {
+                    if(item.InnerText.Contains("Level") == true)
+                    {
+                        Console.WriteLine("{0}", item.InnerText);
+                        talent.Level = item.InnerText;
+                    }
+                    if(item.InnerHtml.Contains("png") == true)
+                    {
+
+                        //Console.WriteLine("Skill Image Link : {0}", item.OuterHtml);
+
+
+
+                        Regex regex = new Regex(@"(?:\/\/.*\.(?:png|jpg))");
+                        Match match = regex.Match(item.OuterHtml);
+                        talent.SkillImageUrl = "http:" + match.Value;
+
+                        talent.Name = item.NextSibling.InnerText;
+                        talent.Desc = item.NextSibling.NextSibling.InnerText;
+                        talent.Popularity = item.NextSibling.NextSibling.NextSibling.NextSibling.InnerText;
+                        talent.Winrate = item.NextSibling.NextSibling.NextSibling.NextSibling.NextSibling.InnerText;
+                        SelectedHero.Tal.Add(talent.Clone()as Talent);
+                        //talent = null;
+                    }
+                }
+            }
+
+            Console.WriteLine("{0}", TalentCountbyNum(20));
+
+        }
+        static public int TalentCountbyNum(int level)
+        {
+            int cnt = 0;
+
+            foreach (var tal in SelectedHero.Tal)
+            {
+                if(tal.Level == "Level: "+level.ToString())
+                {
+                    cnt++;
+                }
+            }
+
+            return cnt;
+        }
+    }
+
+
+    public sealed class DynamicJsonConverter : JavaScriptConverter
+    {
+        public override object Deserialize(IDictionary<string, object> dictionary, Type type, JavaScriptSerializer serializer)
+        {
+            if (dictionary == null)
+                throw new ArgumentNullException("dictionary");
+
+            return type == typeof(object) ? new DynamicJsonObject(dictionary) : null;
+        }
+
+        public override IDictionary<string, object> Serialize(object obj, JavaScriptSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IEnumerable<Type> SupportedTypes
+        {
+            get { return new ReadOnlyCollection<Type>(new List<Type>(new[] { typeof(object) })); }
+        }
+
+        #region Nested type: DynamicJsonObject
+
+        private sealed class DynamicJsonObject : DynamicObject
+        {
+            private readonly IDictionary<string, object> _dictionary;
+
+            public DynamicJsonObject(IDictionary<string, object> dictionary)
+            {
+                if (dictionary == null)
+                    throw new ArgumentNullException("dictionary");
+                _dictionary = dictionary;
+            }
+
+            public override string ToString()
+            {
+                var sb = new StringBuilder("{");
+                ToString(sb);
+                return sb.ToString();
+            }
+
+            private void ToString(StringBuilder sb)
+            {
+                var firstInDictionary = true;
+                foreach (var pair in _dictionary)
+                {
+                    if (!firstInDictionary)
+                        sb.Append(",");
+                    firstInDictionary = false;
+                    var value = pair.Value;
+                    var name = pair.Key;
+                    if (value is string)
+                    {
+                        sb.AppendFormat("{0}:\"{1}\"", name, value);
+                    }
+                    else if (value is IDictionary<string, object>)
+                    {
+                        new DynamicJsonObject((IDictionary<string, object>)value).ToString(sb);
+                    }
+                    else if (value is ArrayList)
+                    {
+                        sb.Append(name + ":[");
+                        var firstInArray = true;
+                        foreach (var arrayValue in (ArrayList)value)
+                        {
+                            if (!firstInArray)
+                                sb.Append(",");
+                            firstInArray = false;
+                            if (arrayValue is IDictionary<string, object>)
+                                new DynamicJsonObject((IDictionary<string, object>)arrayValue).ToString(sb);
+                            else if (arrayValue is string)
+                                sb.AppendFormat("\"{0}\"", arrayValue);
+                            else
+                                sb.AppendFormat("{0}", arrayValue);
+
+                        }
+                        sb.Append("]");
+                    }
+                    else
+                    {
+                        sb.AppendFormat("{0}:{1}", name, value);
+                    }
+                }
+                sb.Append("}");
+            }
+
+            public override bool TryGetMember(GetMemberBinder binder, out object result)
+            {
+                if (!_dictionary.TryGetValue(binder.Name, out result))
+                {
+                    // return null to avoid exception.  caller can check for null this way...
+                    result = null;
+                    return true;
+                }
+
+                result = WrapResultObject(result);
+                return true;
+            }
+
+            public override bool TryGetIndex(GetIndexBinder binder, object[] indexes, out object result)
+            {
+                if (indexes.Length == 1 && indexes[0] != null)
+                {
+                    if (!_dictionary.TryGetValue(indexes[0].ToString(), out result))
+                    {
+                        // return null to avoid exception.  caller can check for null this way...
+                        result = null;
+                        return true;
+                    }
+
+                    result = WrapResultObject(result);
+                    return true;
+                }
+
+                return base.TryGetIndex(binder, indexes, out result);
+            }
+
+            private static object WrapResultObject(object result)
+            {
+                var dictionary = result as IDictionary<string, object>;
+                if (dictionary != null)
+                    return new DynamicJsonObject(dictionary);
+
+                var arrayList = result as ArrayList;
+                if (arrayList != null && arrayList.Count > 0)
+                {
+                    return arrayList[0] is IDictionary<string, object>
+                        ? new List<object>(arrayList.Cast<IDictionary<string, object>>().Select(x => new DynamicJsonObject(x)))
+                        : new List<object>(arrayList.Cast<object>());
+                }
+
+                return result;
+            }
+        }
+
+        #endregion
+    }
+    public class Hero : ICloneable
+    {
+        public string PrimaryName;
+        public string ImageURL;
+        public string AttributeName;
+        public string Group;
+        public string SubGroup;
+        public string Translations;
+
+        public object Clone()
+        {
+            return new Hero()
+            {
+                PrimaryName = this.PrimaryName,
+                ImageURL = this.ImageURL,
+                AttributeName = this.AttributeName,
+                Group = this.Group,
+                SubGroup = this.SubGroup,
+                Translations = this.Translations,
+
+            };
+
+        }
+        public List<Talent> Tal = new List<Talent>();
+    }
+    public class Talent : ICloneable
+    {
+        public string Level = ""; //n특
+        public string Name =""; //이름
+        public string Desc =""; //설명
+        public string Popularity = ""; //선호도
+        public string Winrate = ""; //승률
+        public string SkillImageUrl = "";
+        public object Clone()
+        {
+            return new Talent() {
+                Level = this.Level,
+                Name = this.Name,
+                Desc = this.Desc,
+                Popularity = this.Popularity,
+                Winrate = this.Winrate,
+                SkillImageUrl = this.SkillImageUrl
+            };
+         }
+    }
+}
